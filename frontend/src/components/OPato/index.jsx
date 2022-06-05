8d3992d8ab5d5e7688174798d7e9fce50f314334
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-06-04 21:41:33 -0300

    Logo do pato

---
+++ frontend/src/components/OPato/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { OPatoImg } from "./style";
+
+function OPato() {
+  return <OPatoImg src="./opato.png" />;
+}
+
+export default OPato;
---
+++ frontend/src/components/OPato/style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const OPatoImg = styled.img`
+  width: 40px;
+`;
