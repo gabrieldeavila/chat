03ab308353a3125edfc7dcc37a06f7e6e12e56e4
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-06-05 20:03:57 -0300

    Opção de voltar na navbar

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useCallback, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { GlobalContext } from "../../Context";
 import OPato from "../OPato";
 import { NavbarContainer, NavbarWrapper, NavTitle } from "./style";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const { state } = useContext(GlobalContext);
+
+  const handleNavigate = useCallback(() => {
+    navigate("/", { replace: true });
+    state.emit("disconnect");
+  }, []);
+
   return (
-    <NavbarWrapper>
+    <NavbarWrapper onClick={handleNavigate}>
       <NavbarContainer>
         <OPato />
         <NavTitle>Pato Chat</NavTitle>
