41e13deb281cbef93a56bcc09647e738de1fb23f
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-06-04 21:42:18 -0300

    Componentes usados globalmente

---
+++ frontend/src/assets/base.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import { darkBlack, grey, white } from "./../helpers";
+import { BoxShadow } from "./mixins";
+
+export const Container = styled.div`
+  margin-top: 5rem;
+`;
+
+export const Field = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: ${({ alignItems }) => alignItems || "none"};
+  flex-direction: column;
+  gap: 1rem;
+  padding: 0 5rem;
+`;
+
+export const Label = styled.label`
+  font-weight: 600;
+  font-size: 1.2rem;
+  color: ${white};
+  text-align: left;
+`;
+
+export const Input = styled.input`
+  ${BoxShadow};
+  outline: none;
+  border: none;
+  padding: 1rem;
+  border-radius: 5px;
+  height: 25px;
+  background: ${grey};
+`;
+
+export const FieldsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const Button = styled.button`
+  ${BoxShadow};
+  background: ${grey};
+  color: ${darkBlack};
+  cursor: pointer;
+  padding: 1.2rem 7rem;
+  font-weight: 700;
+  font-size: 18px;
+  border-radius: 5px;
+  width: fit-content;
+  border: none;
+  transition: 0.1s ease;
+
+  &:hover {
+    transform: scale(1.05);
+    opacity: 0.5;
+  }
+`;
---
+++ frontend/src/assets/mixins.js
@@ -0,0 +1,5 @@
+import { css } from "styled-components";
+
+export const BoxShadow = css`
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
