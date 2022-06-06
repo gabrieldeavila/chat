1388a9a78daa5f4aab5aa34cba3aefc2497f95ed
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-06-05 21:15:42 -0300

    Bug 503

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require("express");
 
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "https://chat-brown.vercel.app/",
+    origin: "https://chat-brown.vercel.ap",
     methods: ["GET", "POST"],
     credentials: true,
   },
